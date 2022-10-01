064d52c72e8baeca14031443bfa954ba9e06b7bf
Muhammad Essa <imuhammadessa@gmail.com>
2022-10-01 13:45:16 +0500

    Day#05 Pointers

---
+++ pointerToAFunction/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+void display(){
+    cout << "Hello function!";
+}
+
+int main()
+{
+    display();
+    return 0;
+}
---
+++ pointerToMinMax/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int max(int x, int y){
+    return x>y?x:y;
+}
+
+int min(int x, int y){
+    return x<y?x:y;
+}
+
+int main()
+{
+    int (*fp)(int, int);
+
+    fp=max;
+    (*fp)(10,5);
+    cout << fp;
+
+    fp=min;
+    (*fp)(10,5);
+
+    return 0;
+}
